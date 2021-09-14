73603a6280ee638693ca24444463ca85424a4c46
LeToyek <88649781+LeToyek@users.noreply.github.com>
2021-09-14 09:22:39 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Email acc1 = new Email("Joko","Widodo");
-
+        acc1.setMailboxCapacity(500);
+        acc1.display();
     }
 }
